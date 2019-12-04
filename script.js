f636076c58cf84223ba927b35347cfd585769b8f
Andy <pottsab@gmail.com>
2019-12-04 10:58:54 -0500

    awef

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var aText = new Array(
 "Log data encrypted, searching for key...",
 "Encryption cracked.<br />Decrypting",
 ".",".",".",
-"MESSAGE RECOVERED:<br /><style1>Congratulations Jedi Bryan,<br />You have found a powerful ancient device that can re-align the rare kyber crystal to reflect shades of light. In order to shift the crystal's colors, set the device frequency to 125KHz and write these codes to the crystal and behold the power of your lightsaber and unlock the vast library of data contained in your holocron.</style1>",
+"MESSAGE RECOVERED:<style1><br />Congratulations Jedi Bryan,<br />You have found a powerful ancient device that can re-align the rare kyber crystal to reflect shades of light. In order to shift the crystal's colors, set the device frequency to 125KHz and write these codes to the crystal and behold the power of your lightsaber and unlock the vast library of data contained in your holocron.</style1>",
 "<br /><br />Translating to: ENGLISH...",
 "<br /><style2>Congratulations Jedi Bryan,<br />You have found a powerful ancient device that can re-align the rare kyber crystal to reflect shades of light. In order to shift the crystal's colors, set the device frequency to 125KHz and write these codes to the crystal and behold the power of your lightsaber and unlock the vast library of data contained in your holocron.</style2>",
 
